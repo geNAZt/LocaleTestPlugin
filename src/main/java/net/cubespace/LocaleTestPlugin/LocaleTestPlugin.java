2662917ed393bc3638e7173a8d6d923a48bed194
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-12-08 18:12:05 +0100

    Changed Plugin to reflect the latest API Changes

--- src/main/java/net/cubespace/LocaleTestPlugin/LocaleTestPlugin.java
+++ src/main/java/net/cubespace/LocaleTestPlugin/LocaleTestPlugin.java
@@ -1,16 +1,13 @@
 package net.cubespace.LocaleTestPlugin;
 
-import org.bukkit.Locale;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerSettingsChangeEvent;
-import org.bukkit.plugin.LocaleLoadFailedException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
-import java.util.logging.Level;
 
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
@@ -20,16 +17,7 @@ public class LocaleTestPlugin extends JavaPlugin implements Listener {
     private ArrayList<Player> greeted = new ArrayList<Player>();
 
     public void onEnable() {
-        try {
-            getLocaleManager().load(Locale.GERMAN, getResource("language/de_DE.yml"));
-            getLocaleManager().load(Locale.ENGLISH, getResource("language/en_US.yml"));
-
-            getLocaleManager().setDefaultLocale(Locale.ENGLISH);
-        } catch (LocaleLoadFailedException e) {
-            getLogger().log(Level.SEVERE, "Could not load languages", e);
-            setEnabled(false);
-        }
-
+        getLocaleManager().setDefaultLocale(new java.util.Locale("en", "US"));
         getServer().getPluginManager().registerEvents(this, this);
     }
 
