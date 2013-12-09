1bd0fc9613a1348abc23857dcf86f416ac65f7df
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2013-12-09 06:43:52 +0100

    Changed to reflect the latest API

--- src/main/java/net/cubespace/LocaleTestPlugin/LocaleTestPlugin.java
+++ src/main/java/net/cubespace/LocaleTestPlugin/LocaleTestPlugin.java
@@ -6,8 +6,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerSettingsChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.localization.ResourceLoadFailedException;
+import org.bukkit.plugin.localization.ResourceNotLoadedException;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.logging.Level;
 
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
@@ -17,16 +21,27 @@ public class LocaleTestPlugin extends JavaPlugin implements Listener {
     private ArrayList<Player> greeted = new ArrayList<Player>();
 
     public void onEnable() {
-        getLocaleManager().setDefaultLocale(new java.util.Locale("en", "US"));
+        try {
+            getLocaleManager().load(new Locale("en", "US"), "lang/en_US.yml");
+            getLocaleManager().load(new java.util.Locale("de", "DE"), "lang/de_DE.yml");
+
+            getLocaleManager().setDefaultLocale(new java.util.Locale("en", "US"));
+        } catch (ResourceLoadFailedException e) {
+            getLogger().log(Level.SEVERE, "There was an error in loading the Locales", e);
+            setEnabled(false);
+        }
+
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
-    public void onPlayerSettings(PlayerSettingsChangeEvent event) {
+    public void onPlayerSettings(PlayerSettingsChangeEvent event) throws ResourceNotLoadedException {
         if(!greeted.contains(event.getPlayer())) {
-            event.getPlayer().sendMessage(getLocaleManager().translate(event.getPlayer(), "Welcome"));
+            event.getPlayer().sendMessage(getLocaleManager().translate(event.getPlayer(), "Welcome", event.getPlayer().getWorld().getName(), getServer().getOnlinePlayers().length));
             greeted.add(event.getPlayer());
         }
+
+        event.getPlayer().sendMessage(getLocaleManager().translate(event.getPlayer(), "Changed locale"));
     }
 
     @EventHandler
